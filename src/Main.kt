1f533496427887a391c79d97da7a9896595c8b2a
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-05-13 12:45:58 +0530

    added code for count duplicate elements in array

--- src/Main.kt
+++ src/Main.kt
@@ -10,14 +10,17 @@ fun main() {
 //    println(removeWhiteSpacesFromString("   123   abc   456   x y z "))
 //    println(replaceWhiteSpacesFromStringWithCharacter("   123   abc   456   x y z "))
 
-    val array = arrayOf(1,1,2,4,2,3,8,8,8,7,5,5,6,6)
-    array.sort()
-    val newArray = removeDuplicateElementsInArray(array)
+    val array = arrayOf(9,1,2,4,2,1,5,87,12,1,3,2)
+    /*array.sort()*/
+    /*val newArray = */
+//    sortArrayManually(array)
+   val newArray = removeDuplicateElementsInArray(array)
 
-    for (i in newArray){
-        print(i)
-    }
+    /*for (i in array) {
+        println(i)
+    }*/
 
+    countOfDuplicateElementsInArrayForEachElement(array)
 }
 
 fun reverseString(str: String): String {
@@ -43,28 +46,28 @@ fun removeWhiteSpacesFromString(str: String): String {
     for (element in str.trim()) {
         if (element == ' ') {
             newStr += ""
-        }else{
+        } else {
             newStr += element
         }
     }
     return newStr
 }
 
-fun replaceWhiteSpacesFromStringWithCharacter(str : String): String{
+fun replaceWhiteSpacesFromStringWithCharacter(str: String): String {
     var newStr = ""
     var replaceSymbol = '-'
 
-    for (i in str.trim()){
-        if (i == ' '){
+    for (i in str.trim()) {
+        if (i == ' ') {
             newStr += replaceSymbol
-        }else{
-            newStr+=i
+        } else {
+            newStr += i
         }
     }
     return newStr
 }
 
-fun removeDuplicateElementsInArray(arr : Array<Int>): Array<Int>{
+fun removeDuplicateElementsInArray(arr: Array<Int>): Array<Int> {
     var newArray = emptyArray<Int>()
 
     for (i in arr.indices) {
@@ -73,4 +76,55 @@ fun removeDuplicateElementsInArray(arr : Array<Int>): Array<Int>{
         }
     }
     return newArray
+}
+
+fun sortArrayManually(array: Array<Int>): Array<Int> {
+    var i = 0
+
+    while (i < array.size - 1) {
+        if (array[i] > array[i + 1]) {
+            val temp = array[i]
+            array[i] = array[i + 1]
+            array[i + 1] = temp
+            i = -1
+        }
+        i++
+    }
+
+    return array
+}
+
+fun countOfDuplicateElementsInArrayForEachElement(array: Array<Int>){
+
+
+ /*   for (i in array.indices){
+        var countOfDuplicateElements = 0
+        for (j in i + 1..<array.size) {
+                if (array[i] == array[j]){
+                    countOfDuplicateElements+=1
+                }
+        }
+        if (countOfDuplicateElements> 0){
+            println("${array[i]}... ${countOfDuplicateElements+1}")
+        }else{
+            println("${array[i]}... ${0}")
+        }
+    }*/
+
+    for (i in array.indices) {
+        var count = 0
+        if (array[i] != Int.MIN_VALUE) {
+            for (j in i + 1..<array.size) {
+                if (array[i] == array[j]) {
+                    count++
+                    array[j] = Int.MIN_VALUE
+                }
+            }
+            if (count > 0) {
+                println("${array[i]} repeated ${count + 1} times")
+            } else {
+                println("${array[i]} repeated 0 times")
+            }
+        }
+    }
 }
